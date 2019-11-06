9fc988ac55842ecbc3200b6aac6eb08c6858e97a
zhujinxuan <zhujinxuan@gmail.com>
2019-11-05 19:20:00 -0500

    init

--- packages/nipah/src/Form.ts
+++ packages/nipah/src/Form.ts
@@ -1,8 +1,21 @@
 interface FormT<A> {
-    children? : [A];
+    children? : A[];
+    setChildren<B>(c? : B[]) : FormT<B>;
 }
 
 interface Form extends FormT<Form> {
+    lift() : FormT<Form>;
 }
 
+function fmap<A, B>(a : FormT<A>, f: (src: A) => B) {
+    const {children} = a
+    if (children) {
+        return a.setChildren(children.map(f))
+    }
+    return a.setChildren()
+}
 
+function cata<A>(a: Form, b : (_:FormT<A>) => A) : A {
+    const fb : FormT<A> = fmap<Form, A>(a.lift(), (y : Form) : A => cata<A>(y, b))
+    return b(fb)
+}
